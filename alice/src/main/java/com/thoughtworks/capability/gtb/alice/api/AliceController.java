fad1de9d515a2f8d11e1bfff8817393584c63eb6
Leqi <michaelleqihust@gmail.com>
2020-09-23 16:33:00 +0800

    [leqi] feat: add two controllers on each module.

---
+++ alice/src/main/java/com/thoughtworks/capability/gtb/alice/api/AliceController.java
@@ -0,0 +1,22 @@
+package com.thoughtworks.capability.gtb.alice.api;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@RestController
+public class AliceController {
+    private final WebClient webClient;
+
+    public AliceController() {
+        String bobServiceHelloUrl = "http://localhost:8081/hello";
+        this.webClient = WebClient.builder()
+                .baseUrl(bobServiceHelloUrl)
+                .build();
+    }
+
+    @GetMapping
+    public String sayHello() {
+        return webClient.get().retrieve().bodyToMono(String.class).block();
+    }
+}
